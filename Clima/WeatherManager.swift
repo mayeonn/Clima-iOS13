38efd1476ec4d18b6738021b4fa5ada731676592
mayeonn <mayeonn9@gmail.com>
2023-10-25 16:13:05 +0900

    UITextField, OpenWeather API, Networking, JSON decoding

---
+++ Clima/WeatherManager.swift
@@ -0,0 +1,52 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by 김하연 on 2023/10/22.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=c0346cb7e5308e00b5866d35abb8e8d1&units=metric"
+    
+    func fetchWeather(cityName: String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String){
+        //1. Create a URL
+        if let url = URL(string: urlString) {
+        // URL 생성 성공하면 실행될 코드
+            //2. Create a URL Session
+            let session = URLSession(configuration: .default)
+            //3. Give the session a task
+            let task = session.dataTask(with: url){(data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            //4. Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data){
+        let decoder = JSONDecoder()
+        do{
+            let decodeData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodeData.main.temp)
+            print(decodeData.weather[0].description)
+        } catch{
+            print(error)
+        }
+        
+    }
+
+}
